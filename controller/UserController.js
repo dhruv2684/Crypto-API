496394ca0abdc6f23bd52dd6936f48c417e022b8
Dhruvv2023 <dholiyadhruv2684@gmail.com>
2025-06-25 20:53:12 +0530

    Coin Mining Finall0

--- controller/UserController.js
+++ controller/UserController.js
@@ -62,7 +62,7 @@ exports.login = async (req, res) => {
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
 
-        const token = jwt.sign({ userId: user._id, type: "User" }, process.env.JWT_SECRET, { expiresIn: '3d' });
+        const token = jwt.sign({ userId: user._id, type: "User" }, process.env.JWT_SECRET, { expiresIn: '30d' });
 
         res.status(200).json({
             token,
